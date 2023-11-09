3a1e4cdec45a43ff53146edd6681ad9e719c0deb
simon pineda <simonpineda0521@gmail.com>
2023-11-08 19:12:56 -0500

    the chat error had been fixed

--- src/views/chat.js
+++ src/views/chat.js
@@ -205,13 +205,13 @@ function Chat() {
                                                                         {(evt.typeMess !== 'file' & evt.typeMess !== 'text+file') ?
                                                                             <div className="mess" >
                                                                                 <h4 className="messInfo">{evt.from === user.userName ? <>You</> : <>{evt.from}</>} {evt.date}</h4>
-                                                                                {evt.message !== 'null' ? <Linkify className="textMessage">{evt.message}</Linkify> : <></>}
+                                                                                {evt.message !== 'null' & evt.message !== null ? <Linkify className="textMessage">{evt.message}</Linkify> : <></>}
                                                                                 {evt.fileName !== null & evt.fileName !== 'null' ? <img className="imgUploadedByUser" alt='imgUploadedByUser' src={chatsFlies + evt.fileName} /> : <></>}
                                                                             </div>
                                                                             :
                                                                             <div className="mess" >
                                                                                 <h4 className="messInfo">{evt.from === user.userName ? <>You</> : <>{evt.from}</>} {evt.date}</h4>
-                                                                                {evt.message !== 'null' ? <Linkify className="textMessage">{evt.message}</Linkify> : <></>}
+                                                                                {evt.message !== 'null' & evt.message !== null ? <Linkify className="textMessage">{evt.message}</Linkify> : <></>}
                                                                                 {evt.fileName !== null & evt.fileName !== 'null' ?
                                                                                     <button onClick={() => {
                                                                                         const aTag = document.createElement('a')
