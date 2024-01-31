f5f2598f287caa415672d829fcc3ebf50e3c6e5c
Simon Pineda <76760387+simonpine@users.noreply.github.com>
2024-01-31 12:56:18 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // export const urlBase = 'http://localhost:2000'
-export const urlBase = 'clubhub-backend-production.up.railway.app'
+export const urlBase = 'https://clubhub-backend-production.up.railway.app'
 
 //===========================Users
 
